fbb5f9036c8d730bb07a9fb5ed61901563e00283
Virginia Herron <inadream@gmail.com>
2018-01-12 04:24:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
   if(katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    namePostion = [];
+    var namePostion = [];
     for (i = 0; i < katzDeliLine.length; i++) {
       namePostion.push(`${i + 1}. ${katzDeliLine[1]}`)
     }
